a5db5935abf119997aad39745b8e36c9dd2d53c8
Anton Lipich <anton.lipich1@gmail.com>
2021-04-02 23:05:22 +0300

    Init commit

---
+++ lib/console_interface.rb
@@ -0,0 +1,67 @@
+class ConsoleInterface
+  # содержит все файлы из папки figures, помещённые в массив
+  FIGURES =
+      Dir["#{__dir__}/../data/figures/*.txt"].
+      sort.
+      map { |file_name| File.read(file_name) }
+
+  # на вход экземпляр ConsoleInterface принимает экземпляр Game.
+  # Экземпляр ConsoleInterface вывыодит информацию юзеру:
+  # что знает - выводит сам, что не знает - берёт у экземпляра Game
+  def initialize(game)
+    @game = game
+  end
+
+  #выводит в консоль текущее состояние игры
+  def print_out
+    puts <<~END
+      Слово: #{word_to_show}
+      #{figure}
+      Ошибки (#{@game.errors_made}): #{errors_to_show}
+      У вас осталось ошибок: #{@game.errors_allowed}
+
+    END
+
+    if @game.won?
+      puts "Поздравляем, вы выиграли!"
+    elsif @game.lost?
+      puts "Вы проиграли, загаданное слово: #{@game.word}"
+    end
+  end
+
+  # возвращает ту фигуру из массива FIGURES, имя которой
+  # соответствует кол-ву ошибок, которые сделал пользователь
+  def figure
+    FIGURES[@game.errors_made]
+  end
+
+  # получает на вход массив уже угаданных букв, который на месте еще не отгаданных
+  # содержит nil. Метод записывает в result вместо nil два подчеркивания,
+  # а угаданные буквы оставляет без изменения. Возвращает этот массив в виде
+  # строки, где элементы разделены пробелами, например "К О __ О __ __"
+  def word_to_show
+    result =
+      @game.letters_to_guess.map do |letter|
+        if letter == nil
+          "__"
+        else
+          letter
+        end
+      end
+
+    result.join(" ")
+  end
+
+  #получает массив ошибочных букв, склеивает их в строку вида "Х, У"
+  def errors_to_show
+    @game.errors.join(", ")
+  end
+
+  # получает букву из пользовательского ввода, приводит её к верхнему регистру
+  # и возвращает её
+  def get_input
+    print "Введите следующую букву: "
+    letter = gets[0].upcase
+    letter
+  end
+end
---
+++ lib/game.rb
@@ -0,0 +1,81 @@
+class Game
+  #кол-во допустимых ошибок
+  TOTAL_ERRORS_ALLOWED = 7
+
+  # экземпляр на вход получает загаданное слово
+  # задаем переменную экземпляра с буквами загаданного слова
+  # и пустой массив для дальнейшего сбора в него вводимых букв
+  def initialize(word)
+    @letters = word.chars
+    @user_guesses = []
+  end
+
+  # возвращает буквы, которые отсутствуют в @letters(ошибочные)
+  def errors
+    @user_guesses - normalized_letters
+  end
+
+  # возвращает кол-во ошибок, сделанных пользователем
+  def errors_made
+    errors.length
+  end
+
+  # отнимает от допустимого кол-ва ошибок, кол-во сделанных ошибок и
+  # возвращает кол-во ошибок, которые пользователь ещё может сделать
+  def errors_allowed
+    TOTAL_ERRORS_ALLOWED - errors_made
+  end
+
+  #возвращает массив с уже отгаданными буквами
+  def letters_to_guess
+    result =
+      @letters.map do |letter|
+        if @user_guesses.include?(letter)
+          letter
+        else
+          nil
+        end
+      end
+    result
+  end
+
+  # возвращает true, если у пользователя не осталось ошибок
+  # т.е. игра проиграна
+  def lost?
+    errors_allowed == 0
+  end
+
+  def normalize_letter(letter)
+    letter = "И" if letter == "Й"
+    letter = "Е" if letter == "Ё"
+    letter
+  end
+
+  def normalized_letters
+    @letters.map { |letter| normalize_letter(letter) }
+  end
+
+  # возвращает true, если один из методов в условии возвращает true
+  def over?
+    won? || lost?
+  end
+
+  # закидывает в массив букв введенных пользователем передаваемую букву.
+  # если игра не закончена и передаваемая буква отутствует в массиве
+  # введённых букв
+  def play!(letter)
+    if !over? && !@user_guesses.include?(letter)
+      @user_guesses << letter
+    end
+  end
+
+  # возвращает true, если загаданное и введённое слова совпали
+  def won?
+    (@letters - @user_guesses).empty?
+  end
+
+  # возвращает загаданное слово
+  def word
+    @letters.join
+  end
+end
